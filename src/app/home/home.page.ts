76cc4048cfa3edc43de304e148d1d97ca7d4a11f
ajperalt <ajperalt@gmail.com>
2019-02-01 16:18:43 -0300

    actualización de codigo para mostrar ejemplo al banco

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { HttpClient } from  '@angular/common/http';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { InAppBrowser,InAppBrowserOptions} from '@ionic-native/in-app-browser/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements DoCheck {
+
+  
 
   constructor(private http : HttpClient,private iab: InAppBrowser){
 
   }
+  private browser = null;
+  public resultado_pago = "";
   public cliente = {
       "identificador": ""
   };
@@ -32,11 +36,30 @@ export class HomePage {
             var token_base64 = btoa(info_json);
             var url_pago = 'https://pruebasdesarrollo.coomeva.com.co/psehosting/#/access/';
             url_pago = url_pago + token_base64;
-            var options_iab = 'zoom=no';
-            var browser = this.iab.create(url_pago, '_self', options_iab);
-            console.log(browser);
+            var options_iab : InAppBrowserOptions={
+              zoom : 'no',
+              location:'no',
+              hidenavigationbuttons:'yes'
+            }
+            this.browser = this.iab.create(url_pago,'_blank',options_iab);
+            this.browser.on('exit').subscribe(event => {
+              alert("exito con el cerrado");
+            });
+            this.browser.show();
+            
          }, error => {
           console.log(error);
         });
+  }
+
+  ngDoCheck(): void {
+    if(this.browser){
+      this.browser.executeScript({code:"document.body.innerHTML.indexOf('Regresar')"})
+        .then(function(val){
+          this.resultado_pago=val;
+          this.browser.close();
+          this.browser = null;
+        });
+    }
   }
 }
